c4e800b0e2ea3de0b321af0a1419019389c26bec
Arsalan Baig <arsilbbb@gmail.com>
2023-02-01 18:32:26 +0500

    auth controller refresh Token API

--- controllers/authController.js
+++ controllers/authController.js
@@ -11,11 +11,11 @@ exports.login = async (req, res) => {
     const { email, password } = req.body;
     const admin = await Admin.findOne({ where: { email } });
     if (!admin) {
-      return res.status(400).json({ message: 'Incorrect email or password' });
+      return res.status(400).json({ message: 'Incorrect email' });
     }
     const isMatch = await bcrypt.compare(password, admin.password);
     if (!isMatch) {
-      return res.status(400).json({ message: 'Incorrect email or password' });
+      return res.status(400).json({ message: 'Incorrect password' });
     }
     const displayName = admin.firstName + " " + admin.lastName;
     const token = jwt.sign({ id: admin.id }, JWT_SECRET, {
@@ -35,36 +35,45 @@ exports.login = async (req, res) => {
 };
 exports.refreshToken = async (req, res) => {
   try {
-    const { email } = req.body;
-    const admin = await Admin.findOne({ where: { email } });
+    var token = false;
+    if(req.headers.authorization){
+      const headerToken = req.headers.authorization;
+      token = headerToken.split(" ")[1]
+      console.log(token);
+    }
+ 
+    if (!token) {
+      return res.status(401).json({ message: 'No refresh token, authorization denied' });
+    }
+    const decoded = jwt.verify(token, JWT_SECRET);
+    const admin = await Admin.findByPk(decoded.id);
     if (!admin) {
-      return res.status(400).json({ message: 'Incorrect email' });
+      return res.status(401).json({ message: 'Invalid refresh token' });
     }
     const displayName = admin.firstName + " " + admin.lastName;
-    const token = jwt.sign({ id: admin.id }, JWT_SECRET, {
+    const newToken = jwt.sign({ id: admin.id }, JWT_SECRET, {
       expiresIn: 43200
     });
     res.status(200).json({
-      token: token,
-      refToken: token,
-      role: "Admin",
+      token: newToken,
+      role: "admin",
       data: {
         displayName: displayName,
-        email: email
+        email: admin.email
       }
-    }
-    );
+    });
   } catch (error) {
-    res.status(500).json({ message: 'An error occurred while logging in' + error });
+    res.status(400).json({ message: 'Error refreshing token: ' + error.message });
   }
 };
+
 const authenticate = async (req, res, next) => {
   const token = req.header('x-auth-token');
   if (!token) {
     return res.status(401).json({ message: 'No token, authorization denied' });
   }
   try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const decoded = jwt.verify(token, JWT_SECRET);
     req.admin = await Admin.findByPk(decoded.id);
     next();
   } catch (error) {
